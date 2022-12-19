234fc12dde94f523dca7f752b08ff7e28c3055c5
Latokike <81393451+latokike@users.noreply.github.com>
2022-12-18 17:56:17 -0800

    Update `MythOriginsClient`

--- src/main/java/latokike/mythorigins/common/client/MythOriginsClient.java
+++ src/main/java/latokike/mythorigins/common/client/MythOriginsClient.java
@@ -1,13 +1,14 @@
 package latokike.mythorigins.common.client;
 
 import latokike.mythorigins.common.MythOrigins;
-import latokike.mythorigins.common.entities.HarpyFeatherArrowEntity;
-import latokike.mythorigins.common.entities.renderer.HarpyFeatherArrowEntityRenderer;
+import latokike.mythorigins.common.entity.HarpyFeatherArrowEntity;
+import latokike.mythorigins.common.entity.renderer.HarpyFeatherArrowEntityRenderer;
 import latokike.mythorigins.common.registry.MOEntities;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 import net.minecraft.util.Identifier;
 
@@ -30,42 +31,12 @@ public class MythOriginsClient implements ClientModInitializer {
         EntityRendererRegistry.register(MOEntities.FLAME_2,
                 FlyingItemEntityRenderer::new);
 
-        EntityRendererRegistry.register(MOEntities.ARROW_HARPY_FEATHER, (context) -> new HarpyFeatherArrowEntityRenderer(context) {
-            @Override
-            public Identifier getTexture(HarpyFeatherArrowEntity entity) {
-                return HarpyFeatherArrowEntityRenderer.TEXTURE;
-            }
-        });
-    }
-    /*@Environment(EnvType.CLIENT)
-    public void receiveEntityPacket() {
-        ClientSidePacketRegistry.INSTANCE.register(PacketID, (ctx, byteBuf) -> {
-            EntityType<?> et = Registry.ENTITY_TYPE.get(byteBuf.readVarInt());
-            UUID uuid = byteBuf.readUuid();
-            int entityId = byteBuf.readVarInt();
-            Vec3d pos = EntitySpawnPacket.PacketBufUtil.readVec3d(byteBuf);
-            float pitch = EntitySpawnPacket.PacketBufUtil.readAngle(byteBuf);
-            float yaw = EntitySpawnPacket.PacketBufUtil.readAngle(byteBuf);
-            ctx.getTaskQueue().execute(() -> {
-                if (MinecraftClient.getInstance().world == null)
-                    throw new IllegalStateException("Tried to spawn entity in a null world!");
-                Entity e = et.create(MinecraftClient.getInstance().world);
-                if (e == null)
-                    throw new IllegalStateException("Failed to create instance of entity \"" + Registry.ENTITY_TYPE.getId(et) + "\"!");
-                e.updateTrackedPosition(pos);
-                e.setPos(pos.x, pos.y, pos.z);
-                e.setPitch(pitch);
-                e.setBodyYaw(yaw);
-                e.setId(entityId);
-                e.setUuid(uuid);
-                MinecraftClient.getInstance().world.addEntity(entityId, e);
-            });
-            EntityRendererRegistry.register(MOEntities.ARROW_HARPY_FEATHER, (context) -> new HarpyFeatherArrowEntityRenderer(context) {
-                @Override
+        EntityRendererRegistry.register(MOEntities.ARROW_HARPY_FEATHER, (context) -> {
+            return new HarpyFeatherArrowEntityRenderer(context) {
                 public Identifier getTexture(HarpyFeatherArrowEntity entity) {
                     return HarpyFeatherArrowEntityRenderer.TEXTURE;
                 }
-            });
-        }
-    }*/
+            };
+        });
+    }
 }
