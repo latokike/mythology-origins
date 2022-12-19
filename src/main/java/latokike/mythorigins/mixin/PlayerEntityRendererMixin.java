570832ee721ec31a17d3ef25f136c494b4575c75
Latokike <81393451+latokike@users.noreply.github.com>
2022-12-18 17:49:58 -0800

    (somehow) add back the cool head items

--- src/main/java/latokike/mythorigins/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/latokike/mythorigins/mixin/PlayerEntityRendererMixin.java
@@ -1,32 +1,33 @@
 package latokike.mythorigins.mixin;
 
+import latokike.mythorigins.common.entity.renderer.*;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.entity.EntityRendererFactory.Context;
+import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.render.item.HeldItemRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
-
-    public PlayerEntityRendererMixin(Context context, PlayerEntityModel<AbstractClientPlayerEntity> playerModel, float playerScale) {
-        super(context, playerModel, playerScale);
+    public PlayerEntityRendererMixin(EntityRendererFactory.Context ctx, PlayerEntityModel<AbstractClientPlayerEntity> model, float shadowRadius) {
+        super(ctx, model, shadowRadius);
     }
 
-/*
-    @Inject(method = "<init>(Lnet/minecraft/client/render/entity/EntityRenderDispatcher;Z)V", at = @At("RETURN"))
-    private void construct(EntityRenderDispatcher renderDispatcher, boolean slimChar, CallbackInfo cir) {
-        this.addFeature(new HeadFeatureRenderer<>(this));
-        this.addFeature(new WerewolfFeatureRenderer<>(this));
-        this.addFeature(new LionsManeFeatureRenderer<>(this));
-        this.addFeature(new MinotaurHornsFeatureRenderer<>(this));
-        this.addFeature(new HydraHeadsFeatureRenderer<>(this));
-        this.addFeature(new FeatherFeatureRenderer<>(this));
-        this.addFeature(new SnakeFeatureRenderer<>(this));
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void construct(EntityRendererFactory.Context ctx, boolean slim, CallbackInfo ci) {
+        this.addFeature(new WerewolfFeatureRenderer<>(this, ctx.getHeldItemRenderer()));
+        this.addFeature(new LionsManeFeatureRenderer<>(this, ctx.getHeldItemRenderer()));
+        this.addFeature(new MinotaurHornsFeatureRenderer<>(this, ctx.getHeldItemRenderer()));
+        this.addFeature(new HydraHeadsFeatureRenderer<>(this, ctx.getHeldItemRenderer()));
+        this.addFeature(new FeatherFeatureRenderer<>(this, ctx.getHeldItemRenderer()));
+        this.addFeature(new SnakeFeatureRenderer<>(this, ctx.getHeldItemRenderer()));
     }
-*/
 }
